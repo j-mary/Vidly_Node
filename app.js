ce103e313c1ed25a14c2113bf6c094664f24d0fb
Jude Ocheje <ochejejude@gmail.com>
2018-08-25 19:59:11 +0100

    Add genre api route & model

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose')
+const genres = require('./routes/genre-route')
 const express = require('express')
 const app = express()
 
 mongoose.connect('mongodb://localhost:27017/vidly', {useNewUrlParser: true})
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.error('Failed to connect to MongoDB'))
+
+app.use(express.json())
+app.use('/api/genres', genres)
 
 app.get('/', (req, res) => {
   res.send('Vidly')
---
+++ routes/genre-route.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const router = express.Router()
+
+const { Genre, validateGenre } = require('../models/genre-model')
+
+router.get('/', async (req, res) => {
+  const genres = await Genre.find().sort('name')
+  res.send(genres)
+})
+
+router.get('/:id', async (req, res) => {
+  const genre = await Genre.findById(req.params.id)
+  if(!genre) return res.status(404).send('The genre with given ID was not found')
+  res.send(genre)
+})
+
+router.post('/', async (req, res) => {
+  // validate input
+  const {error} = validateGenre(req.body)
+  if(error) return res.status(400).send(error.details[0].message)
+  // create new genre
+  let genre = new Genre({ name: req.body.name })
+  // save & return
+  genre = await genre.save()
+  res.send(genre)
+})
+
+router.put('/:id', async (req, res) => {
+  // validate input
+  const {error} = validateGenre(req.body)
+  if(error) return res.status(400).send(error.details[0].message)
+  // update genre
+  const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, { new: true })
+  if(!genre) return res.status(404).send('The genre with given ID was not found')
+  // return updated genre
+  res.send(genre)
+})
+
+router.delete('/:id', async (req, res) => {
+  const genre = await Genre.findByIdAndRemove(req.params.id)
+  if(!genre) return res.status(404).send('The genre with given ID was not found')
+  res.send(genre)
+})
+
+module.exports = router
