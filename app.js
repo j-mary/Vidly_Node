97e3f70bf1828868079daf098f4a8d3f21394c43
Jude Ocheje <ochejejude@gmail.com>
2018-08-25 22:46:43 +0100

    Added validation for objectId

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const Joi = require('joi')
+Joi.objectId = require('joi-objectid')(Joi)
 const mongoose = require('mongoose')
 const genres = require('./routes/genre-route')
 const customers = require('./routes/customer-route')
