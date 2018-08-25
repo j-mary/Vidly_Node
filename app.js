892857a790fd0b02ddcd8f1cb7233666266df7f4
Jude Ocheje <ochejejude@gmail.com>
2018-08-25 19:25:17 +0100

    Add connection to mongoDB

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
+
+mongoose.connect('mongodb://localhost:27017/vidly', {useNewUrlParser: true})
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.error('Failed to connect to MongoDB'))
 
 app.get('/', (req, res) => {
   res.send('Vidly')
